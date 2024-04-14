5f66a7a78349f4a723c11844a6aeb58f387a6ec0
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-04-14 21:36:58 +0530

    Media Files configured

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -127,6 +127,13 @@ STATIC_ROOT = BASE_DIR /'static'
 STATICFILES_DIRS = [
     'foodOnline_main/static'
 ]
+
+# Media Files Config 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR /'media'
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
