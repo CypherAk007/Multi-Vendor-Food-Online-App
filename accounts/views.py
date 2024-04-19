d8ffd54cf38e9f206c5b94213c193c01b93b7bba
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-04-19 19:43:16 +0530

    Hash Password using create_user from Model class METHOD -2

--- accounts/views.py
+++ accounts/views.py
@@ -10,13 +10,26 @@ def registerUser(request):
         print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
-            # what ever data we have in form will be assigned to user  
+            # Method -1 
+            # # what ever data we have in form will be assigned to user  
+            # password = form.cleaned_data['password']
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # # save the modified user 
+            # user.save()
+
+            # Method -2 
+            # create the user using create_user Method 
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
             password = form.cleaned_data['password']
-            user = form.save(commit=False)
-            user.set_password(password)
+            user = User.objects.create_user(first_name=first_name,last_name=last_name,username=username,email = email,password=password)
             user.role = User.CUSTOMER
-            # save the modified user 
             user.save()
+            print('user created')
             return redirect('registerUser')
     else:
         form = UserForm()
