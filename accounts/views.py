1fa232fa323f132e7bdfb6efa13b7033178e6ce1
ABHISHEK KRISHNA <abhishekkrishna@ABHISHEKs-MacBook-Air.local>
2024-04-19 17:50:55 +0530

    Register user html

--- accounts/views.py
+++ accounts/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def registeruser(request):
-    return HttpResponse('This is user register form')
+    return render(request,'accounts/registerUser.html')
